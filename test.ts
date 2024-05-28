a42d0ce974ebf6531132b09850dad8a875c77379
Inspirelab-Admin <inspirelab.git@gmail.com>
2024-05-28 12:50:53 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -13,4 +13,3 @@ input.onButtonPressed(Button.B, function() {
     LEDTower.sendLEDCommand(LEDTower.getCommandToSend(LEDTower.get2DTowerCoor(0, 0), LEDTower.getLEDDotsString(LEDTower.get3DCoor(2, 0, 1), LEDTower.getLEDDotsString(LEDTower.get3DCoor(0, 3, 3)), LEDTower.getLEDDotsString(LEDTower.get3DCoor(0, 2, 0))), LEDTower.getColorPickerString(0xff0000)), 50)
     LEDTower.getShowCommand(true)
 })
-LEDTower.getStringCommandToSend("Dot|9,0|5,1,0:6,2,0:7,3,0:8,4,0:9,4,1:10,4,2|255,0,255")
